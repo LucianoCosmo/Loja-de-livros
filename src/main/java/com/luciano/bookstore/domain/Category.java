30604f7146b3ba0193fc8d9030a265bbc1ccf2be
Luciano Cosmo <lcsj.luciano@gmail.com>
2022-05-15 19:50:23 -0300

    Finalizando as classes de domínio com as notações do javax persistece

--- src/main/java/com/luciano/bookstore/domain/Category.java
+++ src/main/java/com/luciano/bookstore/domain/Category.java
@@ -1,19 +1,36 @@
 package com.luciano.bookstore.domain;
 
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Category {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
 	private String description;
 	
+	@OneToMany(mappedBy = "category")
 	private List<Book> books = new ArrayList<>();
 
 	public Category() {
 		super();
-		// TODO Auto-generated constructor stub
+
 	}
 
 	public Category(Integer id, String name, String description) {
